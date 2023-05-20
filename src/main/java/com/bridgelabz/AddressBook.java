ae575aa99eaee98efc9e0631b28e384e78086736
sumithanvat <skhanvat@gmail.com>
2023-05-21 00:22:42 +0530

    Added:Ability to Edit Contact

--- src/main/java/com/bridgelabz/AddressBook.java
+++ src/main/java/com/bridgelabz/AddressBook.java
@@ -1,5 +1,6 @@
 package com.bridgelabz;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AddressBook {
@@ -7,7 +8,7 @@ public class AddressBook {
     Scanner sc = new Scanner(System.in);
 
     public ContactPerson createContact() {
-        ContactPerson person = new ContactPerson("sc.next","sc.next","sc.next","sc.next","sc.next","sc.next","sc.next","sc.next");
+        ContactPerson person = new ContactPerson("sc.next", "sc.next", "sc.next", "sc.next", "sc.next", "sc.next", "sc.next", "sc.next");
         System.out.print("Enter First Name: ");
         person.setFirstName(sc.next());
         System.out.print("Enter Last Name: ");
@@ -32,5 +33,49 @@ public class AddressBook {
         ContactPerson contactPerson = createContact();
         System.out.println(contactPerson);
         System.out.println("Contact added successfully");
+    }
+
+    public void editContact() {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<ContactPerson> addressBook1 = new ArrayList<>();
+
+        boolean isContactFound = false;
+        System.out.println("Enter Name to edit Contact");
+        String name = sc.next();
+        for (ContactPerson contactPerson : addressBook1) {
+            if (name.equalsIgnoreCase(contactPerson.getFirstName())) {
+                isContactFound = true;
+                System.out.print("Enter First Name :");
+                contactPerson.setFirstName(sc.next());
+                System.out.print("Enter Last Name :");
+                contactPerson.setLastName(sc.next());
+                System.out.print("Enter Address :");
+                contactPerson.setAddress(sc.next());
+                System.out.print("Enter City :");
+                contactPerson.setCity(sc.next());
+                System.out.print("Enter State :");
+                contactPerson.setState(sc.next());
+                System.out.print("Enter email :");
+                contactPerson.setEmail(sc.next());
+                System.out.print("Enter ZipCode :");
+                contactPerson.setZip(sc.next());
+                System.out.print("Enter Phone Number :");
+                contactPerson.setPhoneNumber(sc.next());
+                System.out.println(contactPerson);
+                break;
+            }
+        }
+        if (isContactFound) {
+            System.out.println("Contact Updated Successfully..");
+        } else {
+            System.out.println("Oops...Contact not found");
+        }
+    }
+    void display() {
+        ArrayList<ContactPerson> addressBook1 = new ArrayList<>();
+
+        for (ContactPerson person : addressBook1) {
+            System.out.println(person);
+        }
     }
 }
--- src/main/java/com/bridgelabz/AddressBookMain.java
+++ src/main/java/com/bridgelabz/AddressBookMain.java
@@ -1,11 +1,38 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class AddressBookMain {
     public static void main(String[] args) {
-        System.out.println("Welcome to AddressBook ");
-        System.out.println("Contact created in AddressBook");
-        AddressBook addressBook=new AddressBook();
-        addressBook.createContact();
-        addressBook.addContact();
+
+        System.out.println("Welcome to Address Book program");
+        AddressBook addressBook = new AddressBook();
+        Scanner sc = new Scanner(System.in);
+
+        boolean flag1 = true;
+        while (flag1) {
+
+            System.out.println("******************\nSelect Option :");
+            System.out.println("1.Add Contact\n2.Edit Contact\n3.Display\n4.Exit");
+            int option = sc.nextInt();
+            switch (option) {
+                case 1:
+                    addressBook.addContact();
+                    break;
+                case 2:
+                    addressBook.editContact();
+                    break;
+                case 3:
+                    addressBook.display();
+                    break;
+                case 4:
+                    flag1 = false;
+                    break;
+                default:
+                    System.out.println(option + " is not valid option");
+                    break;
+            }
+        }
     }
 }
+
